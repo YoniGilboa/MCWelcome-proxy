b8f77a84c57462e9f8ce711d5a852b29a34167a2
YoniGilboa <gilboa.yoni@gmail.com>
2025-10-07 12:39:57 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -293,13 +293,13 @@ module.exports = async function handler(req, res) {
       }
 
       // אם זו הודעת סיכום קרא ל make      
-      if (message === "send_summary" && userData) {
-        await fetchWithTimeout("https://hook.eu2.make.com/35i403axct5gyl2xskvrpjmjflby8rg3", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(userData)
-        }, 10000);
-      }
+      //if (message === "send_summary" && userData) {
+      //  await fetchWithTimeout("https://hook.eu2.make.com/35i403axct5gyl2xskvrpjmjflby8rg3", {
+      //    method: "POST",
+      //    headers: { "Content-Type": "application/json" },
+      //    body: JSON.stringify(userData)
+      //  }, 10000);
+      //}
     }
 
     res.status(200).json({ reply });
