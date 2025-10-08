abfcd7477b57e534f9ea9e92c159e4f760b2b5b8
YoniGilboa <gilboa.yoni@gmail.com>
2025-10-08 14:52:32 +0300

    Update sendToMake.js
    Add comment before sending to Brian

--- api/sendToMake.js
+++ api/sendToMake.js
@@ -2,6 +2,7 @@ export default async function handler(req, res) {
   try {
     const userData = req.body;
 
+    // no credits in Make right now, using webhook.site url instead, just to see that userData is sent and arrived properly
     //const response = await fetch("https://hook.eu2.make.com/35i403axct5gyl2xskvrpjmjflby8rg3", {
     const response = await fetch("https://webhook.site/b7286379-6a76-41a7-b449-4fa0e5fb89fa", {
       method: "POST",
